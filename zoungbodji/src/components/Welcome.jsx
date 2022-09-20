3645a0c1327fdcfc03480faa03a9379e13166254
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-09-21 00:29:09 +0200

    Slide a revoir

--- zoungbodji/src/components/Welcome.jsx
+++ zoungbodji/src/components/Welcome.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 
-
-
-    const slideImages = [
-  {
-     'images/chambre1.jpg': 'Slide 1'
-  },
-  {
-     'images/cuisine1.jpg': 'Slide 2'
-  },
-  {
-     'images/douche1.jpg': 'Slide 3'
-  },
+const slideImages = [
+  'images/chambre 2.jpg',
+  'images/chambre 3.jpg',
+  'images/chambre 5.jpg'
 ];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
 
 const Slideshow = () => {
     return (
-      <div className="slide-container">
-        <Slide>
-         {slideImages.map((slideImage, index)=> (
-            <div className="each-slide" key={index}>
-              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-                <span>{slideImage.caption}</span>
-              </div>
-            </div>
-          ))} 
-        </Slide>
-      </div>
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <span>Slide 1</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
     )
+}
 
-        
-
-      
-    
-  }
   
   export default Slideshow;
   
